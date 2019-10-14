d3eb1de6ce13681b59312839f9808f4ca99b1d67
obamzuro <oleg.bamzurov@gmail.com>
2019-10-14 14:29:42 +0300

    globbing done

--- source/globbing.c
+++ source/globbing.c
@@ -6,7 +6,7 @@
 /*   By: obamzuro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/06 18:19:31 by obamzuro          #+#    #+#             */
-/*   Updated: 2019/10/13 15:34:34 by obamzuro         ###   ########.fr       */
+/*   Updated: 2019/10/14 14:26:19 by obamzuro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,18 +54,106 @@ int glob_wildcard(char *filename, char *pattern)
 	return (0);
 }
 
+char	**glob_interval_reassemble(t_ftvector *intervals)
+{
+	char		**ret;
+	int			i;
+
+	ret = (char **)malloc(sizeof(char *) * (intervals->len + 1));
+	i = 0;
+	while (i < intervals->len)
+	{
+		ret[i] = intervals->elem[i];
+		++i;
+	}
+	ret[i] = NULL;
+	free(intervals->elem);
+	return (ret);
+}
+
+char	**glob_interval_get_intervals(char **pattern)
+{
+	t_ftvector	intervals;
+	char		*interval;
+	char		**ret;
+	char		beg;
+	char		end;
+
+	init_ftvector(&intervals);
+	while (1)
+	{
+		if (**pattern == '\\')
+			++(*pattern);
+		else if (**pattern == ']')
+			break ;
+		if (!**pattern)
+		{
+			free_ftvector(&intervals);
+			return (NULL);
+		}
+		beg = **pattern;
+		++(*pattern);
+		if (**pattern == '-')
+		{
+			++(*pattern);
+			if (**pattern == '\\')
+				++(*pattern);
+			if (!**pattern || **pattern == ']')
+			{
+				free_ftvector(&intervals);
+				return (NULL);
+			}
+			end = **pattern;
+			++(*pattern);
+			interval = (char *)malloc(sizeof(char) * 3);
+			interval[0] = beg;
+			interval[1] = end;
+			interval[2] = '\0';
+		}
+		else
+		{
+			interval = (char *)malloc(sizeof(char) * 2);
+			interval[0] = beg;
+			interval[1] = '\0';
+		}
+		push_ftvector(&intervals, interval);
+	}
+	return (glob_interval_reassemble(&intervals));
+}
+
 int	glob_interval(char *filename, char *pattern)
 {
-	char	begin;
-	char	end;
+	char	**intervals;
+	char	is_inverse;
+	int		i;
 
-	if (!(*pattern + 1) || *(pattern + 2) != '-' || !*(pattern + 3) || *(pattern + 4) != ']')
+	++pattern;
+	is_inverse = 0;
+	if (*pattern == '!')
+		is_inverse = 1;
+	intervals = glob_interval_get_intervals(&pattern);
+	if (!intervals)
 		return (0);
-	begin = *(pattern + 1);
-	end = *(pattern + 3);
-	pattern += 4;
-	if (*filename < begin || *filename > end)
+	i = 0;
+	while (intervals[i])
+	{
+		if (!intervals[i][1] && *filename == intervals[i][0])
+			break ;
+		else if (intervals[i][1] && (*filename >= intervals[i][0] && *filename <= intervals[i][1]))
+			break ;
+		++i;
+	}
+	if (!is_inverse && !intervals[i])
+	{
+		free_double_arr(intervals);
 		return (0);
+	}
+	if (is_inverse && intervals[i])
+	{
+		free_double_arr(intervals);
+		return (0);
+	}
+	free_double_arr(intervals);
 	if (!*(filename + 1) && !*(pattern + 1))
 		return (1);
 //	if (!*(filename + 1))

bcb3af9c07f70a0924609ecbc8e82c4909a4abd0
obamzuro <oleg.bamzurov@gmail.com>
2019-10-20 13:52:04 +0300

    parameter expansion done

--- source/parameter_expansion_dedicated.c
+++ source/parameter_expansion_dedicated.c
@@ -6,7 +6,7 @@
 /*   By: obamzuro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/19 17:12:01 by obamzuro          #+#    #+#             */
-/*   Updated: 2019/10/19 20:12:12 by obamzuro         ###   ########.fr       */
+/*   Updated: 2019/10/20 13:50:43 by obamzuro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,6 +86,7 @@ char		*parameter_expansion_colon(char *key, char **env)
 }
 
 char		*parameter_expansion_sharp_small_character(char *str, char *pattern);
+char		*parameter_expansion_sharp_big_character(char *str, char *pattern);
 
 char		*parameter_expansion_sharp_small_wildchard(char *str, char *pattern)
 {
@@ -101,6 +102,29 @@ char		*parameter_expansion_sharp_small_wildchard(char *str, char *pattern)
 		temp = parameter_expansion_sharp_small_character(str, pattern + 1);
 	if (!temp)
 		temp = parameter_expansion_sharp_small_wildchard(str + 1, pattern);
+	return (temp);
+}
+
+char		*parameter_expansion_sharp_big_wildchard(char *str, char *pattern)
+{
+	char	*temp;
+
+	if (!*str)
+	{
+		if (*(pattern + 1))
+			return (NULL);
+		else
+			return (str);
+	}
+	temp = parameter_expansion_sharp_big_wildchard(str + 1, pattern);
+	if (temp)
+		return (temp);
+	if (!*(pattern + 1))
+		return (str);
+	else if (*(pattern + 1) == '*')
+		temp = parameter_expansion_sharp_big_wildchard(str, pattern + 1);
+	else
+		temp = parameter_expansion_sharp_big_character(str, pattern + 1);
 	return (temp);
 }
 
@@ -122,6 +146,24 @@ char		*parameter_expansion_sharp_small_character(char *str, char *pattern)
 	return (temp);
 }
 
+char		*parameter_expansion_sharp_big_character(char *str, char *pattern)
+{
+	char	*temp;
+
+//	if (!*(pattern + 1))
+//		return (str);
+	if (*str != *pattern)
+		return (NULL);
+	if (!*(pattern + 1))
+		return (str + 1);
+	temp = NULL;
+	if (*(pattern + 1) == '*')
+		temp = parameter_expansion_sharp_big_wildchard(str + 1, pattern + 1);
+	else
+		temp = parameter_expansion_sharp_big_character(str + 1, pattern + 1);
+	return (temp);
+}
+
 char		*parameter_expansion_sharp_small(char *key, char *additional, char **env)
 {
 	char		*value;
@@ -138,8 +180,74 @@ char		*parameter_expansion_sharp_small(char *key, char *additional, char **env)
 		return (ft_strdup(last));
 	else
 		return (ft_strdup(value));
-//	if (!value)
-//		return (ft_strdup(""));
+}
+
+char		*parameter_expansion_sharp_big(char *key, char *additional, char **env)
+{
+	char		*value;
+	char		*last;
+
+	value = get_envs(key, 2, g_shell->internal, env);
+	if (!additional[0])
+		return (ft_strdup(value));
+	else if (additional[0] == '*')
+		last = parameter_expansion_sharp_big_wildchard(value, additional);
+	else
+		last = parameter_expansion_sharp_big_character(value, additional);
+	if (last)
+		return (ft_strdup(last));
+	else
+		return (ft_strdup(value));
+}
+
+char		*parameter_expansion_procent_small(char *key, char *additional, char **env)
+{
+	char		*value;
+	char		*last;
+	char		*temp_value;
+
+	value = get_envs(key, 2, g_shell->internal, env);
+	temp_value = ft_strdup(value);
+	ft_strreverse(temp_value, ft_strlen(temp_value));
+	ft_strreverse(additional, ft_strlen(additional));
+	if (!additional[0])
+		return (temp_value);
+	else if (additional[0] == '*')
+		last = parameter_expansion_sharp_small_wildchard(temp_value, additional);
+	else
+		last = parameter_expansion_sharp_small_character(temp_value, additional);
+	if (last)
+	{
+		free(temp_value);
+		return (ft_strdup(last));
+	}
+	else
+		return (temp_value);
+}
+
+char		*parameter_expansion_procent_big(char *key, char *additional, char **env)
+{
+	char		*value;
+	char		*last;
+	char		*temp_value;
+
+	value = get_envs(key, 2, g_shell->internal, env);
+	temp_value = ft_strdup(value);
+	ft_strreverse(temp_value, ft_strlen(temp_value));
+	ft_strreverse(additional, ft_strlen(additional));
+	if (!additional[0])
+		return (temp_value);
+	else if (additional[0] == '*')
+		last = parameter_expansion_sharp_big_wildchard(temp_value, additional);
+	else
+		last = parameter_expansion_sharp_big_character(temp_value, additional);
+	if (last)
+	{
+		free(temp_value);
+		return (ft_strdup(last));
+	}
+	else
+		return (temp_value);
 }
 
 char		*parameter_expansion_sharp(char *key, char **env)
@@ -150,9 +258,26 @@ char		*parameter_expansion_sharp(char *key, char **env)
 	sharp = ft_strchr(key, '#');
 	*sharp = '\0';
 	additional = sharp + 1;
-//	if (additional[0] == '#')
-//		return (parameter_expansion_error(key, additional, env));
+	if (additional[0] == '#')
+		return (parameter_expansion_sharp_big(key, additional + 1, env));
 	return (parameter_expansion_sharp_small(key, additional, env));
+}
+
+char		*parameter_expansion_procent(char *key, char **env)
+{
+	char	*sharp;
+	char	*additional;
+	char	*ret;
+
+	sharp = ft_strchr(key, '%');
+	*sharp = '\0';
+	additional = sharp + 1;
+	if (additional[0] == '%')
+		ret = parameter_expansion_procent_big(key, additional + 1, env);
+	else
+		ret = parameter_expansion_procent_small(key, additional, env);
+	ft_strreverse(ret, ft_strlen(ret));
+	return (ret);
 }
 
 char		*parameter_expansion_length(char *key, char **env)
@@ -173,5 +298,7 @@ char		*parameter_expansion_dedicated(char *key, char **env)
 		return (parameter_expansion_colon(key, env));
 	else if (ft_strchr(key, '#'))
 		return (parameter_expansion_sharp(key, env));
+	else if (ft_strchr(key, '%'))
+		return (parameter_expansion_procent(key, env));
 	return (ft_strdup(get_envs(key, 2, g_shell->internal, env)));
 }
